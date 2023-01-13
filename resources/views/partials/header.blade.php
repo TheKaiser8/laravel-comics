84444f6de65659d71c172b8a59027a954b6dae1d
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-01-13 19:40:28 +0100

    Aggiunge main-nav in header bottom

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,7 +4,7 @@
         <div class="container d-flex justify-content-end align-items-center">
             <small>DC POWER™ VISA®</small>
             <div class="dropdown d-flex align-items-center ms-5">
-                <button class="ms-dropdown dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <button class="ms-dropdown secondary-dropdown dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                   ADDITIONAL DC SITES
                 </button>
                 <ul class="dropdown-menu">
@@ -17,8 +17,25 @@
     </div>
     <!-- /header top -->
     <!-- header bottom -->
-    <div class="header-bottom container">
+    <div class="header-bottom d-flex justify-space-between align-items-center container">
         <img src="{{ Vite::asset('resources\img\dc-logo.png')}}" alt="DC Comics logo">
+        <nav class="main-nav">
+            <ul class="d-flex">
+                <li><a href="#">CHARACTERS</a></li>
+                <li><a href="#" class="active">COMICS</a></li>
+                <li><a href="#">MOVIES</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">GAMES</a></li>
+                <li><a href="#">COLLECTIBLES</a></li>
+                <li><a href="#">VIDEOS</a></li>
+                <li><a href="#">FANS</a></li>
+                <li><a href="#">NEWS</a></li>
+                <li><a href="#">SHOP</a>
+                    <button class="ms-dropdown primary-dropdown dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    </button>
+                </li>
+            </ul>
+        </nav>
     </div>
     <!-- /header bottom -->
 </header>
