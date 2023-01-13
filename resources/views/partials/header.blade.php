83caf2127d2edb1cc8dce494ad6c922e94f30c62
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-01-13 19:04:41 +0100

    Aggiunge header top completo e logo header bottom

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,24 @@
-<header>
-    <h1>Header</h1>
+<header class="">
+    <!-- header top -->
+    <div class="header-top">
+        <div class="container d-flex justify-content-end align-items-center">
+            <small>DC POWER™ VISA®</small>
+            <div class="dropdown d-flex align-items-center ms-5">
+                <button class="ms-dropdown dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  ADDITIONAL DC SITES
+                </button>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!-- /header top -->
+    <!-- header bottom -->
+    <div class="header-bottom container">
+        <img src="{{ Vite::asset('resources\img\dc-logo.png')}}" alt="DC Comics logo">
+    </div>
+    <!-- /header bottom -->
 </header>
