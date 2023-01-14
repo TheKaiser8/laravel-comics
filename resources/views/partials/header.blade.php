ee7486c5a1237f122594d39274c184d05f538671
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-01-14 15:56:29 +0100

    Add search-bar in header-bottom. Completa header Aggiunge cdn font-awesome in main.blade

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,4 @@
-<header class="">
+<header>
     <!-- header top -->
     <div class="header-top">
         <div class="container d-flex justify-content-end align-items-center">
@@ -17,7 +17,7 @@
     </div>
     <!-- /header top -->
     <!-- header bottom -->
-    <div class="header-bottom d-flex justify-space-between align-items-center container">
+    <div class="header-bottom container d-flex justify-content-between align-items-center">
         <img src="{{ Vite::asset('resources\img\dc-logo.png')}}" alt="DC Comics logo">
         <nav class="main-nav">
             <ul class="d-flex">
@@ -30,12 +30,24 @@
                 <li><a href="#">VIDEOS</a></li>
                 <li><a href="#">FANS</a></li>
                 <li><a href="#">NEWS</a></li>
-                <li><a href="#">SHOP</a>
+                <li>
+                    <a href="#">SHOP</a>
                     <button class="ms-dropdown primary-dropdown dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                     </button>
+                    <div class="dropdown">
+                        <ul class="dropdown-menu ms-dropdown-item">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </div>
                 </li>
             </ul>
         </nav>
+        <form action="" class="search-bar d-flex" role="search">
+            <input type="search" name="" id="" class="search-bar__input" placeholder="Search">
+            <div class="search-bar__icon"><i class="fa-solid fa-magnifying-glass"></i></div>
+        </form>
     </div>
     <!-- /header bottom -->
 </header>
