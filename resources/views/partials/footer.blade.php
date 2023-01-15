66aa308d1d69cd6cd3bc818a1888c3fa8bbb196a
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-01-15 23:54:01 +0100

    Aggiunge footer-top in partial footer di views

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,53 @@
 <footer>
-    <h5>Footer</h5>
+    <div id="footer-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-auto">
+                    <h5>DC COMICS</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Comics</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Videos</a></li>
+                        <li><a href="#">News</a></li>
+                    </ul>
+                    <h5>SHOP</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="#">Shop DC</a></li>
+                        <li><a href="#">Shop DC Collectibles</a></li>
+                    </ul>
+                </div>
+                <div class="col-auto">
+                    <h5>DC</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="#">Terms Of Use</a></li>
+                        <li><a href="#">Privacy policy (New)</a></li>
+                        <li><a href="#">Ad Choices</a></li>
+                        <li><a href="#">Advertising</a></li>
+                        <li><a href="#">Jobs</a></li>
+                        <li><a href="#">Subscriptions</a></li>
+                        <li><a href="#">Talent Workshops</a></li>
+                        <li><a href="#">CPSC Certificates</a></li>
+                        <li><a href="#">Ratings</a></li>
+                        <li><a href="#">Shop Help</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </div>
+                <div class="col-auto">
+                    <h5>SITES</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="#">DC</a></li>
+                        <li><a href="#">MAD Magazine</a></li>
+                        <li><a href="#">DC Kids</a></li>
+                        <li><a href="#">DC Universe</a></li>
+                        <li><a href="#">DC Power Visa</a></li>
+                    </ul>
+                </div>
+                <p class="mb-0">All Site Content TM and © 2020 DC Entertainment, unless otherwise <a href="#" class="text-decoration-none">noted here</a>. All rights reserved.</p>
+                <a href="#" class="text-decoration-none">Cookies Settings</a>
+            </div>
+        </div>
+    </div>
 </footer>
